29236e6ea68262b49df16fa74b1c53621b6bbea2
Chiaki-Suzuki <c.suzuki.eba@gmail.com>
2022-09-14 17:44:30 +0900

    予約フォーム追加

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 let app = new Vue({
   el: '#app',
   data: {
+    // フォーム用
+    name: '',
+    nameErr: false,
+    tel: '',
+    telErr: false,
+    adult: 0,
+    child: 0,
+    time: 0,
+    course: 0,
+    date: '',
+    // レコメンド用
     status: 1,
     answer1: '',
     answer2: '',
@@ -27,6 +38,11 @@ let app = new Vue({
           image: 'image/ojiya.jpg'
         }
       }
+    }
+  },
+  filters: {
+    number_format: function (val) {
+      return val.toLocaleString();
     }
   },
   methods: {
@@ -56,6 +72,37 @@ let app = new Vue({
       this.resultName = '';
       this.resultImg = '';
       this.status = 1;
+    },
+    /*-------------------------
+      フォーム
+    -------------------------*/
+    // 名前のバリデーションチェック
+    nameCheck: function () {
+      if (!this.name.match(/[0-9０-９ -/:-\[-~！-／：-＠［-｀｛-～、-〜”’・]/)) {
+        this.nameErr = false;
+      } else {
+        this.nameErr = true;
+      }
+    },
+    // 電話番号のバリデーションチェック
+    telCheck: function () {
+      if (this.tel.match(/^[0-9]+$/)) {
+        this.telErr = false;
+      } else {
+        this.telErr = true;
+      }
+    },
+    // 日付をYYYY-MM-DD形式に変換
+    formatDate: function (dt) {
+      let y = dt.getFullYear();
+      let m = ('00' + (dt.getMonth() + 1)).slice(-2);
+      let d = ('00' + dt.getDate()).slice(-2);
+      let result = y + '-' + m + '-' + d;
+      return result;
+    },
+    // 合計金額を表示
+    totalPrice: function () {
+      return (parseInt(this.course) * (parseInt(this.adult) + parseInt(this.child)));
     }
   }
 })
